43fc2d268808d0b7050e38dda12d1fbacc9600be
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-27 15:40:18 +1200

    Add Project Status

--- projectpulse/urls.py
+++ projectpulse/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('projects/', AdminProjectsView.as_view(), name="admin-projects"),
-    path('projects/edit/<int:id>/', AdminProjectDetailView.as_view(), name="admin-project-detail"),
+    path('admin/projects/', AdminProjectsView.as_view(), name="admin-projects"),
+    path('admin/projects/view/<int:id>/', AdminProjectDetailView.as_view(), name="admin-project-detail"),
 ]
