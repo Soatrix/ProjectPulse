90b1416ef697d696f177ab551e52edbee2ed42e5
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-27 14:27:03 +1200

    Rename Dependencies to Requirements

--- projectpulse/models.py
+++ projectpulse/models.py
@@ -46,7 +46,7 @@ class Task(models.Model):
     ], default='not_started')
 
     # Task dependencies
-    dependencies = models.ManyToManyField('self', blank=True, related_name='dependents', symmetrical=False)
+    requirements = models.ManyToManyField('self', blank=True, related_name='requirements', symmetrical=False)
 
     # Custom fields
     custom_fields = models.JSONField(default=dict, blank=True, null=True)
