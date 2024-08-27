a9462b463e6b68c6ec299c4f24846cb525bfcdb7
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-27 14:33:27 +1200

    Add Projects View

--- projectpulse/urls.py
+++ projectpulse/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('projects/', AdminProjectsView.as_view(), "admin-projects")
 ]
--- projectpulse/views.py
+++ projectpulse/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView, View
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.text import slugify
+from .models import *
 
 # Create your views here.
+class AdminProjectsView(TemplateView):
+    template_name = "projectadmin/projects.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["PAGE_TITLE"] = 'All Projects'
+        return context
