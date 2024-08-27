8392b8cbae96af8ca324ff5c7ad1cbd177d5e35c
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-27 17:35:03 +1200

    Add Project Filters

---
+++ projectpulse/templatetags/project_filters.py
@@ -0,0 +1,8 @@
+# projectpulse/templatetags/project_filters.py
+from django import template
+
+register = template.Library()
+
+@register.filter
+def count_completed(items):
+    return sum(1 for item in items if item.status == "completed")
