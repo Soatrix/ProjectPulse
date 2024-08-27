6f891199b05064aae7802390bafcce0fc427104b
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-27 14:01:22 +1200

    Add Admin Menus

--- projectpulse/menus.py
+++ projectpulse/menus.py
@@ -1,4 +1,29 @@
 ADMIN_MENU = [
+    {
+      "name": "Project Management",
+      "items": [
+          {
+              "icon": "diagram-project",
+              "label": "Projects",
+              "link": "admin-projects"
+          },
+          {
+              "icon": "bars-progress",
+              "label": "Tasks",
+              "link": "admin-tasks"
+          },
+          {
+              "icon": "circle-exclamation",
+              "label": "Issues",
+              "link": "admin-issues"
+          },
+          {
+              "icon": "code-branch",
+              "label": "Workflows",
+              "link": "admin-workflows"
+          }
+      ]
+    }
 ]
 FRONT_MENU = [
 ]
