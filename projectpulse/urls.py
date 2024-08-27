9c8323613aca7073efdace36b1f59d3e7859f382
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-27 14:35:46 +1200

    Update urls.py

--- projectpulse/urls.py
+++ projectpulse/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('projects/', AdminProjectsView.as_view(), "admin-projects"),
+    path('projects/', AdminProjectsView.as_view(), name="admin-projects"),
 ]
