8b5b19b9e52f98dd168c7e0c60a79103bb8ba3e7
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-27 15:37:30 +1200

    Add Project Detail View

--- projectpulse/urls.py
+++ projectpulse/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('projects/', AdminProjectsView.as_view(), name="admin-projects"),
+    path('projects/edit/<int:id>/', AdminProjectDetailView.as_view(), name="admin-project-detail"),
 ]
--- projectpulse/views.py
+++ projectpulse/views.py
@@ -17,3 +17,10 @@ class AdminProjectsView(TemplateView):
         context["PAGE_TITLE"] = 'All Projects'
         context["PROJECTS"] = Project.objects.all()
         return context
+class AdminProjectDetailView(TemplateView):
+    template_name = "projectadmin/project-detail.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["PROJECT"] = Project.objects.get(pk=self.kwargs.get("id"))
+        context["PAGE_TITLE"] = context["PROJECT"].name
+        return context
