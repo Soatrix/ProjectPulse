4a94299f0657db8b0fa7a33f436e790b3bf6db30
Dan Alexander <admin@randamonium.co.nz>
2024-08-29 18:10:44 +1200

    Fix Mixing Loading ID

--- projectpulse/mixins.py
+++ projectpulse/mixins.py
@@ -6,7 +6,7 @@ from .models import Project
 class ProjectPermissionRequiredMixin:
     def dispatch(self, request, *args, **kwargs):
         # Get the project based on the URL parameter (assuming 'pk' is used)
-        project = get_object_or_404(Project, pk=kwargs.get('pk'))
+        project = get_object_or_404(Project, pk=kwargs.get('id'))
 
         # Check if the user is the owner or a member of the project
         if project.owner != request.user and request.user not in project.members.all():
