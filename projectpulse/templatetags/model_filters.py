49d0c13ae076744297c32a8613ab643bfa0ad1e5
Dan Alexander <admin@randamonium.co.nz>
2024-08-29 22:04:32 +1200

    Update model_filters.py

--- projectpulse/templatetags/model_filters.py
+++ projectpulse/templatetags/model_filters.py
@@ -4,13 +4,12 @@ from django.apps import apps
 register = template.Library()
 
 @register.filter
-def get_object(model_name_id_tuple, object_id):
+def get_object(model_name, object_id):
     """
     Retrieve an object from the model based on its ID.
 
     Usage in template: {{ model_name|get_object:object_id }}
     """
-    model_name, model_id = model_name_id_tuple
     try:
         model = apps.get_model(app_label='projectpulse', model_name=model_name)
         return model.objects.get(id=object_id)
