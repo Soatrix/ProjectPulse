688af1735f6b9ad3cf7e1c9f41a97bdb0391f90b
Fibbicles <71574715+Fibbicles@users.noreply.github.com>
2024-08-31 18:19:25 +1200

    Prevent Duplicate Activity Logs

--- projectpulse/admin.py
+++ projectpulse/admin.py
@@ -20,29 +20,6 @@ class ProjectAdmin(admin.ModelAdmin):
     search_fields = ('name', 'owner__username')
     list_filter = ('status', 'start_date', 'end_date')
 
-    def save_model(self, request, obj, form, change):
-        if change:  # If the object is being changed (not created)
-            changes = self.get_changes(obj)
-            ActivityLog.objects.create(
-                model_name=obj.__class__.__name__,
-                object_id=obj.pk,
-                action_type=ActivityLog.ActionType.UPDATED,
-                changed_by=request.user,
-                changes=changes
-            )
-        super().save_model(request, obj, form, change)
-
-    def get_changes(self, obj):
-        changes = {}
-        old_obj = obj.__class__.objects.get(pk=obj.pk)
-        for field in obj._meta.fields:
-            field_name = field.name
-            old_value = getattr(old_obj, field_name)
-            new_value = getattr(obj, field_name)
-            if old_value != new_value:
-                changes[field_name] = {'old': old_value, 'new': new_value}
-        return changes
-
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     inlines = [TaskNoteInline]
@@ -50,58 +27,12 @@ class TaskAdmin(admin.ModelAdmin):
     search_fields = ('name', 'project__name', 'assignee__username')
     list_filter = ('status', 'start_date', 'due_date')
 
-    def save_model(self, request, obj, form, change):
-        if change:  # If the object is being changed (not created)
-            changes = self.get_changes(obj)
-            ActivityLog.objects.create(
-                model_name=obj.__class__.__name__,
-                object_id=obj.pk,
-                action_type=ActivityLog.ActionType.UPDATED,
-                changed_by=request.user,
-                changes=changes
-            )
-        super().save_model(request, obj, form, change)
-
-    def get_changes(self, obj):
-        changes = {}
-        old_obj = obj.__class__.objects.get(pk=obj.pk)
-        for field in obj._meta.fields:
-            field_name = field.name
-            old_value = getattr(old_obj, field_name)
-            new_value = getattr(obj, field_name)
-            if old_value != new_value:
-                changes[field_name] = {'old': old_value, 'new': new_value}
-        return changes
-
 @admin.register(Issue)
 class IssueAdmin(admin.ModelAdmin):
     inlines = [IssueNoteInline]
     list_display = ('title', 'project', 'task', 'reporter', 'assignee', 'status', 'severity', 'created_at')
     search_fields = ('title', 'project__name', 'task__name', 'reporter__username', 'assignee__username')
     list_filter = ('status', 'severity', 'created_at', 'updated_at')
-
-    def save_model(self, request, obj, form, change):
-        if change:  # If the object is being changed (not created)
-            changes = self.get_changes(obj)
-            ActivityLog.objects.create(
-                model_name=obj.__class__.__name__,
-                object_id=obj.pk,
-                action_type=ActivityLog.ActionType.UPDATED,
-                changed_by=request.user,
-                changes=changes
-            )
-        super().save_model(request, obj, form, change)
-
-    def get_changes(self, obj):
-        changes = {}
-        old_obj = obj.__class__.objects.get(pk=obj.pk)
-        for field in obj._meta.fields:
-            field_name = field.name
-            old_value = getattr(old_obj, field_name)
-            new_value = getattr(obj, field_name)
-            if old_value != new_value:
-                changes[field_name] = {'old': old_value, 'new': new_value}
-        return changes
 
 @admin.register(ActivityLog)
 class ActivityLogAdmin(admin.ModelAdmin):
